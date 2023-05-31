b570ad8243ec9ea7d1b09912317b432df0dd08cd
Sebastian Hadley <71616059+SebastianHadley@users.noreply.github.com>
2023-05-31 21:03:38 +1000

    adds links to faculty

--- web-app/src/components/FacultyStaff.js
+++ web-app/src/components/FacultyStaff.js
@@ -3,7 +3,8 @@ import {  getFacultyImage } from "../Helper";
 export const FacultyStaff = ({ Name, JobDescription, ImageID,Email}) => {
   return (
     <Col size={12} sm={6} md={4}  className="proj-faculty-wrap">
-      <div className="proj-imgbx">
+      <a href={'https://google.com'} style={{ all: 'unset' }}>
+      <div className="proj-imgbx"> 
         <img className="proj-img-content" src= {getFacultyImage(ImageID)}/>
         <div className="proj-txtx">
           <h4>{Name}</h4>
@@ -11,6 +12,7 @@ export const FacultyStaff = ({ Name, JobDescription, ImageID,Email}) => {
           <span className="proj-txtx-span">{Email}</span>
         </div>
       </div>
+      </a>
     </Col>
   )
 }
